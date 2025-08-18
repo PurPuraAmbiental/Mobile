7b9d1c5d039042934686fab7eb2dfa41f80a47ef
Dora3003 <isadorajardim3003@gmail.com>
2025-08-18 10:54:58 -0300

    Criando tela de erro de internet

--- app/src/main/java/com/purpura/app/ui/screens/InternetError.java
+++ app/src/main/java/com/purpura/app/ui/screens/InternetError.java
@@ -1,4 +1,4 @@
-package com.purpura.app.ui;
+package com.purpura.app.ui.screens;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.purpura.app.R;
 
-public class Cadastro extends AppCompatActivity {
+public class InternetError extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_internet_error);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
