b8ae7706d4d122fcd3eeb105137489f920d26862
JhowzinSql <jvabreu262008@gmail.com>
2025-09-12 12:08:19 -0300

    (chore) subindo o gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation("androidx.credentials:credentials:1.5.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.5.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
-
     // AndroidX e Material
     implementation(libs.appcompat)
     implementation(libs.material)
