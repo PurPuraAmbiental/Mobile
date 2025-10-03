490b304f8385932d82c3b4a47cb9ab22f64c9145
Dora3003 <isadorajardim3003@gmail.com>
2025-10-03 11:11:50 -0300

    (visual) Alterando ícones e colocando padrão de cores

--- app/src/main/java/com/purpura/app/ui/screens/MainActivity.java
+++ app/src/main/java/com/purpura/app/ui/screens/MainActivity.java
@@ -5,8 +5,13 @@ import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -36,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        EdgeToEdge.enable(this);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.purple_200));
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this,R.color.purple_200));
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, windowInsets) -> {
+            Insets systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
     }
 
 }
